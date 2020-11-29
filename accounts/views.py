7ca33bc71076314a33645c961cc0f2bb4f93724f
sciencetechrocks <sciencetechrocks@gmail.com>
2020-11-30 09:00:13 +1030

    tut 7: db model queries

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import *
 
 def home(request):
+
     return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 def customer(request):
     return render(request, 'accounts/customer.html')
