8eab4bd05269721620c773d97bd9a3060b1e0805
sciencetechrocks <sciencetechrocks@gmail.com>
2020-11-30 13:51:42 +1030

    tut 11: inline forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
+from django.forms import inlineformset_factory
 
 def home(request):
     orders = Order.objects.all()
@@ -31,17 +32,21 @@ def customer(request, pk):
     context = {'customer': customer, 'orders': orders, 'order_count': order_count, 'customer_id': customer_id}
     return render(request, 'accounts/customer.html', context)
 
-def createOrder(request):
-    form = OrderForm()
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product','status'), extra=5)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none() ,instance=customer)
+    #form = OrderForm(initial={'customer': customer})
 
     if request.method == 'POST':
         #print('Printing POST: ', request.POST)
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        #form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST,instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
 
-    context = {'form': form}
+    context = {'formset': formset}
     return render(request, 'accounts/order_form.html', context)
 
 def updateOrder(request, pk):
